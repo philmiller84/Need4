fc3c2ca0ed699dca1dce27d8d1ef318fd1788cee
Philip Miller <phil.miller84@gmail.com>
2020-07-12 08:05:13 -0700

    Removed Blazor template files

--- XYZ/Data/Need4Service.cs
+++ XYZ/Data/Need4Service.cs
@@ -32,21 +32,5 @@ namespace XYZ.Data
             itemClient = new ItemRepository.ItemRepositoryClient(channel);
             tradeClient = new TradeService.TradeServiceClient(channel);
         }
-
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-        {
-            var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
-        }
     }
 }
